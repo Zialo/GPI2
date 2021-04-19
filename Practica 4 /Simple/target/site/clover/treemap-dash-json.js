2b5c2abee5f21cd6ea8cd8d803404262ef0a96bd
Hodei Zia <hodei54@gmail.com>
2021-04-19 18:32:29 +0000

    Practica 5: OpenClover

---
+++ Practica 4 /Simple/target/site/clover/treemap-dash-json.js
@@ -0,0 +1,6 @@
+var treeMapJson = {"id":"Clover database lun. abr. 19 2021 18:19:44 UTC0","name":"","data":{
+    "$area":4.0,"$color":0.0,"title":" 4 Elements, 0% Coverage"},"children":[
+    {"id":"Simple0","name":"Simple","data":{"$area":4.0,"$color":0.0,"title":
+        "Simple 4 Elements, 0% Coverage"},"children":[]}]}
+;
+processTreeMapDashJson (treeMapJson);
---
+++ Practica 4 /Simple/target/site/clover/treemap-json.js
@@ -0,0 +1,9 @@
+var treeMapJson = {"id":"Clover database lun. abr. 19 2021 18:19:44 UTC0","name":"","data":{
+    "$area":4.0,"$color":0.0,"title":" 4 Elements, 0% Coverage"},"children":[
+    {"id":"Simple0","name":"Simple","data":{"$area":4.0,"$color":0.0,"title":
+        "Simple 4 Elements, 0% Coverage"},"children":[{"id":"App0","name":
+          "App","data":{"$area":4.0,"$color":0.0,"path":
+            "Simple/App.html#App","title":"App 4 Elements, 0% Coverage"},
+          "children":[]}]}]}
+;
+processTreeMapJson (treeMapJson);
